d24f903fe5556d63736f01d38fccd0fbea98cc2b
Adam <adam.fafera@edu.uekat.pl>
2026-05-13 03:12:03 +0200

    Add signout confirmation dialogs users and restaurant owners

--- backend/menumate/obj/Release/net8.0/menumate.AssemblyInfo.cs
+++ backend/menumate/obj/Release/net8.0/menumate.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("menumate")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bc6e381e45de159eeb6a115fb3594f4b9b1ce26")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cdefe47ba6ff1a0a55e3b02da83da9da3ea5507")]
 [assembly: System.Reflection.AssemblyProductAttribute("menumate")]
 [assembly: System.Reflection.AssemblyTitleAttribute("menumate")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
